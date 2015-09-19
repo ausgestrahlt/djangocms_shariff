7002822544fcee4dba933a969e90655da521bb5c
creimers <christoph@superservice-international.com>
2015-09-19 18:07:07 +0200

    add use_backend setting

--- djangocms_shariff/models.py
+++ djangocms_shariff/models.py
@@ -4,13 +4,23 @@ from cms.models import CMSPlugin
 from django.utils.encoding import python_2_unicode_compatible
 
 
-
+@python_2_unicode_compatible
 class Shariff(CMSPlugin):
-  facebook = models.BooleanField()
-  twitter = models.BooleanField()
-  emailboolean = models.BooleanField()
-  emailsubject = models.TextField()
-  emailadress = models.CharField(max_length=255)
+    # general settings
+    use_backend = models.BooleanField(default=False)
+    # TODO: add theme, orientation
 
-  def __str__(self):
-    return '' 
+    # social media channels
+    facebook = models.BooleanField()
+    twitter = models.BooleanField()
+
+    # email
+    # TODO: rename emailboolean to email
+    emailboolean = models.BooleanField()
+    # TODO: renmae emailsubject to email_subject
+    emailsubject = models.TextField()
+    # TODO: erase emailaddress, include email_body
+    emailadress = models.CharField(max_length=255)
+
+    def __str__(self):
+        return 'Shariff' 
