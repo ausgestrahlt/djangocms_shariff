96b45705e478a0b70353c5d169778722488123a2
Christoph Reimers <christoph@superservice-international.com>
2017-05-05 13:41:05 +0200

    add threema

---
+++ djangocms_shariff/migrations/0002_auto_20170505_1340.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangocms_shariff', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='shariff',
+            name='threema',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AlterField(
+            model_name='shariff',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(parent_link=True, related_name='djangocms_shariff_shariff', auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin'),
+        ),
+        migrations.AlterField(
+            model_name='shariff',
+            name='info',
+            field=models.BooleanField(default=False, help_text='Show info icon next to share buttons'),
+        ),
+        migrations.AlterField(
+            model_name='shariff',
+            name='use_backend',
+            field=models.BooleanField(default=False, help_text='The backend will fetch sharing statistics for the respective channels', verbose_name='Use the shariff backend'),
+        ),
+    ]
--- djangocms_shariff/models.py
+++ djangocms_shariff/models.py
@@ -1,13 +1,12 @@
-from django.db import models
+import json
 
+from django.db import models
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
-
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from cms.models import CMSPlugin 
-import json
+from cms.models import CMSPlugin
 
 
 @python_2_unicode_compatible
@@ -15,79 +14,88 @@ class Shariff(CMSPlugin):
     # general settings
     use_backend = models.BooleanField(
         _('Use the shariff backend'),
-        default=False
+        default=False,
+        help_text=_('The backend will fetch sharing statistics for the respective channels')
     )
 
     ORIENTATION_CHOICES = (
         ('vertical', _('vertical')),
-        ('horizontal',_('horizontal')),
+        ('horizontal', _('horizontal')),
         )
     orientation_choices = models.CharField(
         _('orientation'),
         max_length=10,
         choices=ORIENTATION_CHOICES,
-        default='horizontal')
-    
+        default='horizontal'
+        )
+
     THEME_CHOICES = (
-        ('standard',_('standard')),
-        ('grey',_('grey')),
-        ('white',_('white')),
+        ('standard', _('standard')),
+        ('grey', _('grey')),
+        ('white', _('white')),
         )
     theme_choices = models.CharField(
         _('theme'),
         max_length=8,
         choices=THEME_CHOICES,
-        default='standard')
+        default='standard'
+        )
 
     # social media channels
+    addthis = models.BooleanField(default=False)
     facebook = models.BooleanField(default=False)
-    twitter = models.BooleanField(default=False)
     googleplus = models.BooleanField(default=False)
     linkedin = models.BooleanField(default=False)
     pinterest = models.BooleanField(default=False)
-    xing = models.BooleanField(default=False)
-    whatsapp = models.BooleanField(default=False)
-    addthis = models.BooleanField(default=False)
+    threema = models.BooleanField(default=False)
     tumblr = models.BooleanField(default=False)
-    info = models.BooleanField(default=False, help_text=_('Provide information about the Plugin'))
+    twitter = models.BooleanField(default=False)
+    whatsapp = models.BooleanField(default=False)
+    xing = models.BooleanField(default=False)
+
+    info = models.BooleanField(
+        default=False,
+        help_text=_('Show info icon next to share buttons')
+    )
 
     # email
     mail = models.BooleanField(help_text=_('Enable sharing via e-mail'))
-    mail_subject = models.TextField(_('subject'),blank=True)
-    mail_body = models.TextField(_('content'),blank=True)
+    mail_subject = models.TextField(_('subject'), blank=True)
+    mail_body = models.TextField(_('content'), blank=True)
 
     def clean(self):
         if self.use_backend:
             try:
                 backend = reverse('shariff_backend:get')
             except Exception, e:
+                msg = 'It seems the backend is not configured properly.'
                 raise ValidationError(
                     {
-                        'use_backend': _('It seems the backend is not configured properly.')
+                        'use_backend': _(msg)
                     }
                 )
 
     def get_social_choices(self):
-      channel_dict = {}
-      channel_dict['facebook'] = self.facebook
-      channel_dict['twitter'] = self.twitter
-      channel_dict['xing'] = self.xing
-      channel_dict['googleplus'] = self.googleplus
-      channel_dict['linkedin'] = self.linkedin
-      channel_dict['pinterest'] = self.pinterest
-      channel_dict['whatsapp'] = self.whatsapp
-      channel_dict['addthis'] = self.addthis
-      channel_dict['tumblr'] = self.tumblr
-      channel_dict['info'] = self.info
-      channel_dict['mail'] = self.mail
-      channel_pseudo_list = '['
-      for key in channel_dict.keys():
-        if channel_dict[key]:
-          channel_pseudo_list += "&quot;%s&quot;," % key
+        channel_dict = {}
+        channel_dict['facebook'] = self.facebook
+        channel_dict['twitter'] = self.twitter
+        channel_dict['xing'] = self.xing
+        channel_dict['googleplus'] = self.googleplus
+        channel_dict['linkedin'] = self.linkedin
+        channel_dict['pinterest'] = self.pinterest
+        channel_dict['whatsapp'] = self.whatsapp
+        channel_dict['addthis'] = self.addthis
+        channel_dict['tumblr'] = self.tumblr
+        channel_dict['info'] = self.info
+        channel_dict['mail'] = self.mail
+        channel_pseudo_list = '['
+        for key in channel_dict.keys():
+            if channel_dict[key]:
+                channel_pseudo_list += "&quot;%s&quot;," % key
 
-      channel_pseudo_list += ']'
-      channel_pseudo_list=channel_pseudo_list.replace(',]',']')
-      return channel_pseudo_list
-    
-    def __str__(self): 
-        return 'Shariff' 
+        channel_pseudo_list += ']'
+        channel_pseudo_list = channel_pseudo_list.replace(',]', ']')
+        return channel_pseudo_list
+
+    def __str__(self):
+        return 'Shariff'
