d30f6c73a57c87405545a37cd281e88ee0599049
Dimitri <dev@yare.fr>
2020-08-09 13:06:13 +0200

    on_delete in migrations

--- djangocms_shariff/migrations/0002_auto_20170505_1340.py
+++ djangocms_shariff/migrations/0002_auto_20170505_1340.py
@@ -6,29 +6,41 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('djangocms_shariff', '0001_initial'),
-    ]
+    dependencies = [("djangocms_shariff", "0001_initial")]
 
     operations = [
         migrations.AddField(
-            model_name='shariff',
-            name='threema',
+            model_name="shariff",
+            name="threema",
             field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
-            model_name='shariff',
-            name='cmsplugin_ptr',
-            field=models.OneToOneField(parent_link=True, related_name='djangocms_shariff_shariff', auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin'),
+            model_name="shariff",
+            name="cmsplugin_ptr",
+            field=models.OneToOneField(
+                on_delete=django.db.models.deletion.CASCADE,
+                parent_link=True,
+                related_name="djangocms_shariff_shariff",
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                to="cms.CMSPlugin",
+            ),
         ),
         migrations.AlterField(
-            model_name='shariff',
-            name='info',
-            field=models.BooleanField(default=False, help_text='Show info icon next to share buttons'),
+            model_name="shariff",
+            name="info",
+            field=models.BooleanField(
+                default=False, help_text="Show info icon next to share buttons"
+            ),
         ),
         migrations.AlterField(
-            model_name='shariff',
-            name='use_backend',
-            field=models.BooleanField(default=False, help_text='The backend will fetch sharing statistics for the respective channels', verbose_name='Use the shariff backend'),
+            model_name="shariff",
+            name="use_backend",
+            field=models.BooleanField(
+                default=False,
+                help_text="The backend will fetch sharing statistics for the respective channels",
+                verbose_name="Use the shariff backend",
+            ),
         ),
     ]
