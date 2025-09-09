8190bf888978080a7036b0febfe15e218f6a3d17
Christoph Reimers <christoph@superservice-international.com>
2025-09-09 12:10:37 +0200

    url fixes

--- djangocms_shariff/urls.py
+++ djangocms_shariff/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DataBackendView.as_view(), name='get'),
+    re_path(r'^$', views.DataBackendView.as_view(), name='get'),
 ]
